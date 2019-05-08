af8f216cddd628991d0b7f0037f0fa00e430d727
IOntUMAtIp <rhsolive@gmail.com>
2019-05-08 18:17:56 -0300

    Update to Hain 0.7.0-beta.1

--- hain/tools/chocolateyinstall.ps1
+++ hain/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
-﻿$ErrorActionPreference = 'Stop';
+$ErrorActionPreference = 'Stop';
 
 $packageName = 'hain'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 $installerType = 'exe'
-$url = 'https://github.com/hainproject/hain/releases/download/v0.6.6/HainSetup-ia32-v0.6.6.exe'
-$checksum = 'A7DA87019AC4D0D9F62BF9AA82E0A37DF0EA3E56C9C578605D7CF328D4D39E88'
+$url = 'https://github.com/hainproject/hain/releases/download/v0.7.0-beta.1/Hain-ia32-v0.7.0-beta.1.zip'
+$checksum = '6072e0ef4f00a3f671287451de25665cc12749fd8dffc5c51b9a4504570b817a'
 $checksumType = 'sha256'
-$url64 = 'https://github.com/hainproject/hain/releases/download/v0.6.6/HainSetup-x64-v0.6.6.exe'
-$checksum64 = '59B048C9FB4E635DBE7FBEB1576C62D25FA7A58E84CFF709F228D97961F6378D'
+$url64 = 'https://github.com/hainproject/hain/releases/download/v0.7.0-beta.1/HainSetup-x64-v0.7.0-beta.1.exe'
+$checksum64 = 'e49e938ec09066de1bf9972b3afee98fa08872a4fb2fd274588b40e898aa1a69'
 $checksumType64 = 'sha256'
 $silentArgs = '/VERYSILENT'
 $validExitCodes = @(0) 
@@ -22,4 +22,4 @@ Install-ChocolateyPackage `
   -Url64bit "$url64" `
   -Checksum64 "$checksum64" `
   -ChecksumType64 "$checksumType64" `
-  -ValidExitCodes "$validExitCodes"
+-ValidExitCodes "$validExitCodes"
